eb010f2b2018314a6c16c3a562d78dd1df9c8afa
Tymoteusz Bartnik <tymoteusz.bartnik@machineportal.com>
2021-12-12 12:46:28 +0100

    Add mathNet, fastBitmap

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.wrapper = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.wrapper)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wrapper
+            // 
+            this.wrapper.Location = new System.Drawing.Point(12, 12);
+            this.wrapper.Name = "wrapper";
+            this.wrapper.Size = new System.Drawing.Size(563, 426);
+            this.wrapper.TabIndex = 0;
+            this.wrapper.TabStop = false;
+            this.wrapper.Paint += new System.Windows.Forms.PaintEventHandler(this.wrapper_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.wrapper);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.wrapper)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private PictureBox wrapper;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,7 @@
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+using MathNet.Numerics.LinearAlgebra;
+
 namespace gk_p4
 {
     public partial class Form1 : Form
@@ -5,6 +9,32 @@ namespace gk_p4
         public Form1()
         {
             InitializeComponent();
+
+
+            var m = Matrix<double>.Build.Dense(4, 4);
+
+            m[0, 0] = 1;
+            m[1, 3] = 1;
+            m[1, 1] = 1;
+            m[2, 2] = 1;
+            m[3, 3] = 1;
+            m[3, 0] = 1;
+
+            var m1 = Matrix<double>.Build.Dense(4, 4);
+
+            m1[0, 0] = 1;
+            m1[0, 3] = 1;
+            m1[1, 2] = 1;
+            m1[2, 1] = 1;
+            m1[3, 0] = 1;
+            m1[3, 3] = 1;
+
+            Debug.WriteLine(m * m1);
+        }
+
+        private void wrapper_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
